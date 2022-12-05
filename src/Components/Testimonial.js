4cd8a629006be9c7a469c052132a3a9dbbb99f96
gumgum <ronaldovolkert@gmail.com>
2022-12-05 21:47:23 +0700

    first commit

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -6,59 +6,59 @@ const Testimonial = () => {
   return (
     <div className="tBase">
       <div className="allbox">
-      <div className="tTbox">
-              <h3>Testimonial</h3>
-              <h5> Berbagai Review Positif dari Para Pelanggan Kami</h5>
-      </div>
-      <div className="container">
-        <div className="containerCol">
-          <div className="photoframe">
-            <img src={Photo1} />
-          </div>
-          <div className="itemtext">
-            <img src={Rate} />
-            <h5>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod”
-            </h5>
-            <p>John Dee 32, Bromo</p>
-          </div>
+        <div className="tTbox">
+          <h3>Testimonial</h3>
+          <h5> Berbagai Review Positif dari Para Pelanggan Kami</h5>
         </div>
+        <div className="container">
+          <div className="containerCol">
+            <div className="photoframe">
+              <img src={Photo1} />
+            </div>
+            <div className="itemtext">
+              <img src={Rate} />
+              <h5>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod”
+              </h5>
+              <p>John Dee 32, Bromo</p>
+            </div>
+          </div>
 
-        <div className="containerCol">
-          <div className="photoframe">
-            <img src={Photo2} />
+          <div className="containerCol">
+            <div className="photoframe">
+              <img src={Photo2} />
+            </div>
+            <div className="itemtext">
+              <img src={Rate} />
+              <h5>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod”
+              </h5>
+              <p>John Dee 32, Bromo</p>
+            </div>
           </div>
-          <div className="itemtext">
-            <img src={Rate} />
-            <h5>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod”
-            </h5>
-            <p>John Dee 32, Bromo</p>
-          </div>
-        </div>
 
-        <div className="containerCol">
-          <div className="photoframe">
-            <img src={Photo1} />
-          </div>
-          <div className="itemtext">
-            <img src={Rate} />
-            <h5>
-              “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod lorem ipsum dolor sit amet, consectetur adipiscing
-              elit, sed do eiusmod”
-            </h5>
-            <p>John Dee 32, Bromo</p>
+          <div className="containerCol">
+            <div className="photoframe">
+              <img src={Photo1} />
+            </div>
+            <div className="itemtext">
+              <img src={Rate} />
+              <h5>
+                “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                sed do eiusmod lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod”
+              </h5>
+              <p>John Dee 32, Bromo</p>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     </div>
   );
